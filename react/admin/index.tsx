77d83cfea1f561d6c58511f1a897d5aa63b8107b
codad5 <aniezeoformic@gmail.com>
2024-08-17 20:30:07 +0100

    fix plugin main page

--- react/admin/index.tsx
+++ react/admin/index.tsx
@@ -1,4 +1,4 @@
-// import { __ } from '@wordpress/i18n';
+import { __ } from '@wordpress/i18n';
 import ReactRender from "../helper-components/react-wrapper";
 import '@/styles/sass/admin.scss';
 import React from 'react';
@@ -8,7 +8,7 @@ ReactRender(({...props}) => {
 	const helpfulLinks = [
 		{
 			name: "Documentation",
-			link: "/ultimate-crypto-widget",
+			link: props?.PluginURI ?? "https://ultimatecryptowidget.com"
 		},
 		{
 			name: "Support",
@@ -36,10 +36,10 @@ ReactRender(({...props}) => {
 						<img src={props.png_logo} alt="logo" width="200" height="200"/>
 					</div>
 					<div className={`ucwp-admin-welcome-message`} >
-						{/* <p> { __( 'Welcome to the Ultimate Crypto Widget plugin.', 'ultimate-crypto-widget' ) } </p> */}
-						{/*short description about the plugin*/}
-						{/* <p> { __( 'This plugin allows you to display the current price of any cryptocurrency in your website.', 'ultimate-crypto-widget' ) } </p> */}
-						{/* helpful links */}
+						 <p> { __( 'Welcome to the Ultimate Crypto Widget plugin.', 'ultimate-crypto-widget' ) } </p>
+						short description about the plugin
+						 <p> { __( 'This plugin allows you to display the current price of any cryptocurrency in your website.', 'ultimate-crypto-widget' ) } </p>
+						 helpful links
 						<div className={`ucwp-admin-helpful-links`}>
 							{helpfulLinks.map((link, index) => (
 								<a key={index} href={link.link} target="_blank" rel="noreferrer">{link.name}</a>
