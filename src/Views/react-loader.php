386473680518856aa8439a80375a797954d01272
codad5 <aniezeoformic@gmail.com>
2024-08-24 16:15:18 +0100

    escaping

--- src/Views/react-loader.php
+++ src/Views/react-loader.php
@@ -1,7 +1,7 @@
 <?php if(!defined('ABSPATH')) exit(); ?>
 
 <?php if(isset($react_id)) : ?>
-	<div id="<?php echo  $react_id ?>">Loading...</div>
+	<div id="<?php echo  esc_attr($react_id) ?>">Loading...</div>
 <?php else : ?>
-	<div class="ucwp-react-load-failed">Failed to load the react component (<?php echo $component ?? '' ?>)</div>
+	<div class="ucwp-react-load-failed">Failed to load the react component (<?php echo esc_html($component ?? '') ?>)</div>
 <?php endif; ?>
