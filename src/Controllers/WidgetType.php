5157d2f24bc21cc7f1735ee8e9b10c5857ee6c26
codad5 <aniezeoformic@gmail.com>
2024-08-31 02:35:38 +0100

    fix conversion

--- src/Controllers/WidgetType.php
+++ src/Controllers/WidgetType.php
@@ -100,7 +100,7 @@ final class WidgetType {
 			// possible setting param to be set below
 		}
 		$passable_setting_param['currency_symbol'] = $settings['currency_symbol'] ?? Currency::get_symbol($passable_setting_param['default_currency']);
-		$passable_setting_param['usd_conversion_rate'] = (int) OpenExchangeHelper::convert($passable_setting_param['default_currency'], 'USD', 1);
+		$passable_setting_param['usd_conversion_rate'] = (int) OpenExchangeHelper::convert( 'USD', $passable_setting_param['default_currency'],1);
 		$pass = [
 			'settings' => $passable_setting_param,
 			...$data_param
